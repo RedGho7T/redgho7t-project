ee033e36aedebf96ea787528a6dc8f86b25da287
RedGho7T <redgho7t@gmail.com>
2025-08-13 02:33:38 +0300

    As part of the test, I created a database in MySQL and connected it to the IDE + added the logic for setting up the connection to the database in the Util class.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    private Util() {
+    }
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Ошибка подключения к базе данных: " + e.getMessage(), e);
+        }
+    }
 }
